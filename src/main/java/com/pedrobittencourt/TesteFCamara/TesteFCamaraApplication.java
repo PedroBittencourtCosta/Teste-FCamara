341ccb5931c8ff7f284b5ab0dc6886358962c1ee
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2024-02-01 19:12:05 -0300

    feat: add base

---
+++ src/main/java/com/pedrobittencourt/TesteFCamara/TesteFCamaraApplication.java
@@ -0,0 +1,13 @@
+package com.pedrobittencourt.TesteFCamara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFCamaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFCamaraApplication.class, args);
+	}
+
+}
